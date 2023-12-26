9d11d879621d3057fe5711e31b1d207e856e1d9c
faizan86agency <faizan.86agency@gmail.com>
2023-12-26 16:06:58 +0530

    initial commit

---
+++ lib/cjs/Generic.ts
@@ -0,0 +1,3 @@
+const generic = () => {};
+
+module.exports = generic;
---
+++ lib/esm/Generic.d.ts
@@ -0,0 +1,43 @@
+import { GoogleAuthOptions } from "google-auth-library";
+import { Pagination } from "./types/generic/Pagination";
+import { GenericClass } from "./types/generic/GenericClass";
+import { GenericObject } from "./types/generic/GenericObject";
+
+export declare class GenericClient {
+  private readonly httpClient;
+  private readonly baseUrl;
+  private readonly classUrl;
+  private readonly objectUrl;
+  constructor(credentials: GoogleAuthOptions["credentials"]);
+  getClasses(
+    issuerId: string,
+    token?: string,
+    maxResults?: number
+  ): Promise<{
+    resources: GenericClass[];
+    pagination: Pagination;
+  }>;
+  getClass(
+    issuerId: string,
+    classId: string
+  ): Promise<GenericClass | undefined>;
+  createClass(classObject: GenericClass): Promise<GenericClass>;
+  updateClass(classObject: GenericClass): Promise<GenericClass>;
+  patchClass(classObject: GenericClass): Promise<GenericClass>;
+  getObjects(
+    issuerId: string,
+    classId: string,
+    token?: string,
+    maxResults?: number
+  ): Promise<{
+    resources: GenericObject[];
+    pagination: Pagination;
+  }>;
+  getObject(
+    issuerId: string,
+    objectId: string
+  ): Promise<GenericObject | undefined>;
+  createObject(object: GenericObject): Promise<GenericObject>;
+  updateObject(object: GenericObject): Promise<GenericObject>;
+  patchObject(object: GenericObject): Promise<GenericObject>;
+}
